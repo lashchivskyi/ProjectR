63bda8e11cb5c7d55f931fe53cd912fa568e3140
Serhii <lashchivskyi@gmail.com>
2020-02-06 11:00:16 +0200

    lesson37

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import * as serviceWorker from "./serviceWorker";
-import state, { cubscribe } from "./redux/State";
+import store from "./redux/State";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { addPost, updateNewPostText } from "./redux/State";
 import { BrowserRouter } from "react-router-dom";
 
 let rerenderEntireTree = state => {
@@ -12,16 +11,16 @@ let rerenderEntireTree = state => {
     <BrowserRouter>
       <App
         state={state}
-        addPost={addPost}
-        updateNewPostText={updateNewPostText}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
       />
     </BrowserRouter>,
     document.getElementById("root")
   );
 };
 
-rerenderEntireTree(state);
-cubscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.cubscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/State.js
+++ src/redux/State.js
@@ -1,53 +1,54 @@
-let rerenderEntireTree = () => {
-  console.log("State chainged");
-};
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hi,haw yu doing", likesCount: 234 },
-      { id: 2, message: "Hello", likesCount: 134 }
-    ],
-    newPostText: "it-kam.com"
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi,haw yu doing", likesCount: 234 },
+        { id: 2, message: "Hello", likesCount: 134 }
+      ],
+      newPostText: "it-kam.com"
+    },
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: "Serhii" },
+        { id: 2, name: "Vasya" },
+        { id: 3, name: "Igor" },
+        { id: 4, name: "Valera" },
+        { id: 5, name: "Vova" }
+      ],
+      messages: [
+        { id: 1, message: "Hi.Men" },
+        { id: 2, message: "Hello" },
+        { id: 3, message: "Hi,Men" },
+        { id: 4, message: "how you doing?" },
+        { id: 5, message: "how you doing?" }
+      ]
+    }
   },
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: "Serhii" },
-      { id: 2, name: "Vasya" },
-      { id: 3, name: "Igor" },
-      { id: 4, name: "Valera" },
-      { id: 5, name: "Vova" }
-    ],
-    messages: [
-      { id: 1, message: "Hi.Men" },
-      { id: 2, message: "Hello" },
-      { id: 3, message: "Hi,Men" },
-      { id: 4, message: "how you doing?" },
-      { id: 5, message: "how you doing?" }
-    ]
+  getState() {
+    return this._state;
+  },
+  _callSubscriber() {
+    console.log("State chainged");
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0
+    };
+    this._state.profilePage.posts.push(newPost); // добавляєм пост
+    this._state.profilePage.newPostText = "";
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText; // добавляєм пост
+    this._callSubscriber(this._state);
+  },
+  cubscribe(observer) {
+    this._callSubscriber = observer;
   }
 };
 
-window.state = state;
+window.store = store;
 
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 1
-  };
-  state.profilePage.posts.push(newPost); // добавляєм пост
-  message: state.profilePage.newPostText = "";
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = newText => {
-  state.profilePage.newPostText = newText; // добавляєм пост
-  rerenderEntireTree(state);
-};
-
-export const cubscribe = observer => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
